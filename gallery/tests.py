2354b58be61b88cae8b5e62f93f4d3c3afa6090e
seanans <maks1998ivan@gmail.com>
2025-05-23 13:02:17 +0300

    Add tests.py

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Image
+from datetime import datetime, timedelta
 
-# Create your tests here.
+class GalleryViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.image = Image.objects.create(
+            title="Test Image",
+            image="test.jpg",
+            created_date=datetime.now().date(),
+            age_limit=18
+        )
+
+    def test_gallery_view_shows_recent_images(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Image")
+
+    def test_gallery_view_excludes_old_images(self):
+        old_image = Image.objects.create(
+            title="Old Image",
+            image="old.jpg",
+            created_date=(datetime.now() - timedelta(days=31)).date(),
+            age_limit=18
+        )
+        response = self.client.get(reverse('main'))
+        self.assertNotContains(response, "Old Image")
+
+class ImageDetailViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.image = Image.objects.create(
+            title="Detail Image",
+            image="detail.jpg",
+            created_date=datetime.now().date(),
+            age_limit=18
+        )
+
+    def test_image_detail_view(self):
+        response = self.client.get(reverse('image_detail', args=[self.image.pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Detail Image")
